57ad0d7619e9a5462340d7b6a74028aa3363dcce
till <till@php.net>
2009-08-31 14:15:45 +0000

    example

---
+++ docs/example.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'Services/Amazon/EC2/InstanceManager.php';
+
+$access_key_id     = 'YOUR KEY';
+$access_key_secret = 'YOUR SECRET';
+
+$manager = new Services_Amazon_EC2_InstanceManager(
+    $access_key_id,
+    $access_key_secret
+);
+
+$runner = $manager->getRunner($amazon_machine_image);
+$runner->setNumber($num);
+$runner->setUserData($data);
+$instances = $runner->runInstances();
+
+foreach ($instances as $instance) {
+    echo $instance->getId(), "\n";
+}
+
+$manager->terminateInstances($instances);
+
+?>
