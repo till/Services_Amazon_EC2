42a75fd490f88d45d9cce29e63570285541d8e39
Till Klampaeckel <till@php.net>
2009-12-11 17:54:24 +0100

    better to be explicit

--- docs/show-instances.php
+++ docs/show-instances.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'Services/Amazon/EC2/InstanceManager.php';
 
-$access_key_id     = '';
-$access_key_secret = '';
+$access_key_id     = 'YOUR KEY';
+$access_key_secret = 'YOUR SECRET';
 
 $manager = new Services_Amazon_EC2_InstanceManager(
     $access_key_id,
